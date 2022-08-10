a25ed1ea498580bb77491d9ae95452483c937bb9
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-08-10 14:12:51 -0700

    Added Client::InitializeEnet()

--- NumberGuessingGame/Client.cpp
+++ NumberGuessingGame/Client.cpp
@@ -1,7 +1,18 @@
 #include "Client.h"
+#include <enet/enet.h>
+#include <iostream>
+
+using std::cout; using std::endl;
 
 void Client::InitializeEnet()
 {
+	if (enet_initialize() < 0)
+	{
+		cout << "Failed to initialize enet." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	atexit(enet_deinitialize);
 }
 
 void Client::KbListen()
