74d0faf142f5e80c7f1293baf49485bfb3c54464
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-08-10 12:06:47 -0700

    Created header file for Server object.

--- NumberGuessingGame/Server.h
+++ NumberGuessingGame/Server.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <enet/enet.h>
+
 class Server
 {
+	int m_winningNumber = 0;
+
+	void InitialieEnet();
+
+	void HandleConnect(ENetEvent* e);
+	void HandleReceive(ENetEvent* e);
+	void HandleDisconnect(ENetEvent* e);
+	
+	void HandleGuess(ENetPeer* p, int guess);
+	void RespondIncorrectGuess(ENetPeer* p);
+	void RespondCorrectGuess(ENetPeer* p);
+
+	void RandomizeNumber();
+	void NewGame();
+
+	void ServerLoop();
+public:
+	void StartServer();
 };
 
