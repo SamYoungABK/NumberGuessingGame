1c75a6af7616aef8522c30678cea2cd8c7ad1fc7
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-08-10 13:19:54 -0700

    added Packet::Serialize()

--- NumberGuessingGame/Packet.cpp
+++ NumberGuessingGame/Packet.cpp
@@ -1,1 +1,37 @@
 #include "Packet.h"
+#include <enet/enet.h>
+
+
+Packet::Packet()
+{
+}
+
+char* Packet::Serialize()
+{
+    char* serializedPacket;
+    int packetSize;
+
+    packetSize = sizeof(m_type) +
+        sizeof(m_dataLength) +
+        strlen(m_data);
+
+    serializedPacket = new char[packetSize+1];
+
+    memcpy_s(&serializedPacket+0, 1, &m_type, 1);
+    memcpy_s(&serializedPacket+1, 1, &m_dataLength, 1);
+    memcpy_s(&serializedPacket+2, m_dataLength, &m_data, m_dataLength);
+
+    return serializedPacket;
+}
+
+void Packet::Deserialize(char* serializedPacket)
+{
+}
+
+void Packet::SendToPeer(ENetPeer* p)
+{
+}
+
+void Packet::Broadcast()
+{
+}
--- NumberGuessingGame/Packet.h
+++ NumberGuessingGame/Packet.h
@@ -1,5 +1,31 @@
 #pragma once
-class Packet
+#include <enet/enet.h>
+
+enum class PacketType : char
 {
+	GUESS = 'g',
 };
 
+class Packet
+{
+	PacketType m_type;
+	char m_dataLength = 0;
+	char* m_data = nullptr;
+
+public:
+	Packet();
+
+	void SetType(PacketType type) { m_type = type; }
+	PacketType GetType() { return m_type; }
+
+	void setData(char* data)
+	{
+		m_dataLength = strlen(m_data);
+		m_data = data;
+	}
+
+	char* Serialize();
+	void Deserialize(char* serializedPacket);
+	void SendToPeer(ENetPeer* p);
+	void Broadcast();
+};
