76c4f963e70220bf1bd2fc8d67c6f9d0ce61d2fc
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-08-10 14:11:58 -0700

    Basic Client header file set up

--- NumberGuessingGame/Client.cpp
+++ NumberGuessingGame/Client.cpp
@@ -1,1 +1,45 @@
 #include "Client.h"
+
+void Client::InitializeEnet()
+{
+}
+
+void Client::KbListen()
+{
+}
+
+void Client::StartThreads()
+{
+}
+
+void Client::HandleConnect()
+{
+}
+
+void Client::HandleReceive()
+{
+}
+
+void Client::HandleDisconnect()
+{
+}
+
+void Client::SendGuess()
+{
+}
+
+void Client::DisplayMessage()
+{
+}
+
+void Client::ClientLoop()
+{
+}
+
+void Client::SetAddress()
+{
+}
+
+void Client::StartClient()
+{
+}
--- NumberGuessingGame/Client.h
+++ NumberGuessingGame/Client.h
@@ -1,5 +1,39 @@
 #pragma once
+
+#include <enet/enet.h>
+#include <thread>
+
 class Client
 {
+	std::thread* m_inputThread = nullptr;
+	ENetAddress m_serverAddress;
+
+	ENetHost* client;
+
+	void InitializeEnet();
+	void KbListen();
+	void StartThreads();
+
+	void HandleConnect();
+	void HandleReceive();
+	void HandleDisconnect();
+
+	void SendGuess();
+	void DisplayMessage();
+
+	void ClientLoop();
+public:
+	Client()
+	{
+		enet_address_set_host(&m_serverAddress, "localhost");
+		m_serverAddress.port = 1234;
+	}
+
+	Client(ENetAddress addr) :
+		m_serverAddress(addr)
+	{}
+
+	void SetAddress();
+	void StartClient();
 };
 
