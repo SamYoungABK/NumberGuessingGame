99a55c2ff95cfa8b68bd1468df473e8858758aff
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-08-10 14:47:51 -0700

    Added client startup options to main

--- NumberGuessingGame/main.cpp
+++ NumberGuessingGame/main.cpp
@@ -1,17 +1,23 @@
 #include <enet/enet.h>
 #include "Menu.h"
 #include "Server.h"
+#include "Client.h"
 
 int main()
 {
 	Menu menu({ "Start Server", "Start Client"});
-	Server s;
 	menu.DisplayPrompt();
+
+	Server s;
+	Client c;
 
 	switch (menu.GetSelectedOption())
 	{
-	case '1':
+	case 1:
 		s.StartServer();
+		break;
+	case 2:
+		c.StartClient();
 		break;
 	default:
 		break;
